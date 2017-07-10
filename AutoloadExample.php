518754d0ab320691f5e649f4fecfb1b222c1c5f8
root <root@testcluster.ngs.ru.zlrd.d>
2017-07-10 15:36:32 +0700

    comments version 0.0.0

---
+++ AutoloadExample.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace zlrdn\comments;
+
+/**
+ * This is just an example.
+ */
+class AutoloadExample extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "Hello!";
+    }
+}
